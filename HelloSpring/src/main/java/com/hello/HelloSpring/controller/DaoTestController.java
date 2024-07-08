34a581c626215c9e0e4662c90c62163fea9a0fc3
안지애 <rozioh@naver.com>
2024-07-08 12:19:58 +0900

    DaoTestController 입력, 수정, 삭제 추가 및 상수 정의 클래스 생성

--- HelloSpring/src/main/java/com/hello/HelloSpring/controller/DaoTestController.java
+++ HelloSpring/src/main/java/com/hello/HelloSpring/controller/DaoTestController.java
@@ -6,10 +6,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hello.hellospring.common.Constants;
 import com.hello.hellospring.common.bean.MemberBean;
 import com.hello.hellospring.common.daos.MemberDao;
 
@@ -59,4 +61,78 @@ public class DaoTestController {
 		return map;
 	}
 	
+	@RequestMapping(value = "/insertMember", method = {RequestMethod.POST})
+	@ResponseBody
+	public Map<String, Object> insertMember(@RequestBody MemberBean bean) throws Exception{
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		//validation 체크
+		if(bean.getBirthdate() != null && bean.getBirthdate().length() > 8) {
+			map.put(Constants.RESULT_KEY, Constants.RESULT_VAL_FAIL);
+			map.put(Constants.RESULT_KEY_MSG, "생년월일이 8글자를 초과할 수 없습니다.");
+			return map;
+		}
+		
+		int res = memberDao.insertMember(bean);
+		if(res > 0) {
+			map.put(Constants.RESULT_KEY, Constants.RESULT_VAL_OK);
+			map.put(Constants.RESULT_KEY_MSG, "회원등록에 성공하였습니다.");
+		}else {
+			map.put(Constants.RESULT_KEY, Constants.RESULT_VAL_FAIL);
+			map.put(Constants.RESULT_KEY_MSG, "회원등록 실패하였습니다.");
+		}
+		map.put("memberList", res);
+		
+		return map;
+	}
+	
+	@RequestMapping(value = "/updateMember", method = {RequestMethod.POST})
+	@ResponseBody
+	public Map<String, Object> updateMember(@RequestBody MemberBean bean) throws Exception{
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String result = Constants.RESULT_VAL_FAIL;
+		String resultMsg = "실패 하였습니다.";
+		
+		//TODO
+		
+		int res = memberDao.updateMember(bean);
+		if(res > 0) {
+			result = Constants.RESULT_VAL_OK;
+			resultMsg = "회원정보 수정에 성공하였습니다.";		
+		}
+		
+		map.put(Constants.RESULT_KEY, result);
+		map.put(Constants.RESULT_KEY_MSG, resultMsg);
+		return map;
+	}
+	
+	@RequestMapping(value = "/deleteMember", method = {RequestMethod.POST})
+	@ResponseBody
+	public Map<String, Object> deleteMember(@RequestBody MemberBean bean) throws Exception{
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String result = Constants.RESULT_VAL_FAIL;
+		String resultMsg = "실패 하였습니다.";
+		
+		//TODO
+		try {
+			int res = memberDao.deleteMember(bean);
+			if(res > 0) {
+				// 성공
+				result = Constants.RESULT_VAL_OK;
+				resultMsg = "회원정보 삭제에 성공하였습니다.";		
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+//			resultMsg = e.getMessage(); // 정보노출이 된다.
+			resultMsg = "서버쪽 에러가 발생했습니다. 관리자에게 문의하세요.";
+		}
+		
+		
+		map.put(Constants.RESULT_KEY, result);
+		map.put(Constants.RESULT_KEY_MSG, resultMsg);
+		return map;
+	}
 }
