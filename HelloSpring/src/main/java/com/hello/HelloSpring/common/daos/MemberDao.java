48c01d8d8ab66d83c802bb6cd8fd73ffa99bbf8e
안지애 <rozioh@naver.com>
2024-07-08 10:00:31 +0900

    MemberDao 입력, 수정, 삭제 추가

--- HelloSpring/src/main/java/com/hello/HelloSpring/common/daos/MemberDao.java
+++ HelloSpring/src/main/java/com/hello/HelloSpring/common/daos/MemberDao.java
@@ -9,12 +9,21 @@ import com.hello.hellospring.common.bean.MemberBean;
 @Mapper
 public interface MemberDao {
 	
-	// 1건 조회
+	// 1건 조회 R
 	public MemberBean selectMember(MemberBean mBean);
 	
-	// 여러 건 조회
+	// 여러 건 조회 R
 	public List<MemberBean> selectMemberList(MemberBean bean);
 	
-	// 검색 조회
+	// 검색 조회 R
 	public List<MemberBean> selectMemberSearch(MemberBean bean);
+	
+	// 1건 입력 int인 것은 결과 row 수 확인려고 함. C
+	public int insertMember(MemberBean bean);
+	
+	// 1건 수정 U
+	public int updateMember(MemberBean bean);
+	
+	// 1건 삭제 D
+	public int deleteMember(MemberBean bean);
 }
