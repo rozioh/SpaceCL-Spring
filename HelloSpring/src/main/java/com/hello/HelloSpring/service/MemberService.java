b96e470839409283a3b2db6dfc02bae153b64d9d
안지애 <rozioh@naver.com>
2024-07-08 12:46:54 +0900

    MemberService 구현
    - deleteMember 진행할때 board에 작성한 글이 있으면 삭제되지 않는 부분을 처리하기 위해 @Transaction 구현.
    - 테스트 시 에러 발생했을 때 DB RollBack되지 않고 Board 데이터가 삭제되는 현상 발생.

--- HelloSpring/src/main/java/com/hello/HelloSpring/service/MemberService.java
+++ HelloSpring/src/main/java/com/hello/HelloSpring/service/MemberService.java
@@ -2,7 +2,9 @@ package com.hello.hellospring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.hello.hellospring.common.bean.BoardBean;
 import com.hello.hellospring.common.bean.MemberBean;
 import com.hello.hellospring.common.daos.BoardDao;
 import com.hello.hellospring.common.daos.MemberDao;
@@ -10,6 +12,7 @@ import com.hello.hellospring.common.daos.MemberDao;
 // 서비스가 하는 일은?
 // 서비스는 여러 개의 DAO 객체를 관리해서 일을 수행하는 클래스이다.
 @Service
+@Transactional
 public class MemberService {
 	
 	@Autowired
@@ -17,15 +20,20 @@ public class MemberService {
 	@Autowired
 	private BoardDao boardDao;
 	
+	@Transactional
 	public int deleteMember(MemberBean memberBean) throws Exception{
 		int res = 0;
 		
+		BoardBean boardBean = new BoardBean();
+		boardBean.setMemberNo( memberBean.getMemberNo() );
+		
 		// 1. Board 테이블에서 삭제
-		// boardDao.deleteBoard()
+		boardDao.deleteBoardFromMemberNo(boardBean);
 		
 		// 2. Member 테이블에서 삭
 //		memberDao.deleteMember()
+		throw new Exception("일부러 에러 일으킨다."); //테스트
 		
-		return res;
+//		return res;
 	}
 }
