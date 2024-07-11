e5833ea300f6a8d8d426245f3f442bf1e4353935
안지애 <rozioh@naver.com>
2024-07-11 14:24:25 +0900

    'TokenCheckInterceptor` 토큰 체크 인터셉터 추가, `WebMvcConfig` 웹 설정 클래스에서 인터셉터 등록

--- HelloSpring/src/main/java/com/hello/hellospring/common/interceptor/TokenCheckInterceptor.java
+++ HelloSpring/src/main/java/com/hello/hellospring/common/interceptor/TokenCheckInterceptor.java
@@ -1,30 +1,84 @@
 package com.hello.hellospring.common.interceptor;
 
-import org.springframework.web.context.request.WebRequestInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
+import java.io.PrintWriter;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hello.hellospring.common.Constants;
+import com.hello.hellospring.common.utils.JwtTokenHelper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class TokenCheckInterceptor extends WebRequestHandlerInterceptorAdapter{
-
-	public TokenCheckInterceptor(WebRequestInterceptor requestInterceptor) {
-		super(requestInterceptor);
-	}
+/**
+ * 토큰 체크 인터셉터
+ */
+public class TokenCheckInterceptor implements HandlerInterceptor{
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		return super.preHandle(request, response, handler);
+		System.out.println("TokenInterceptor preHandle() ===> " + request.getRequestURI());
+		
+		String result = Constants.RESULT_VAL_FAIL;
+		String resultMsg = "회원정보 Token 검증에 실패 하였습니다.";
+		
+		// 헤더로부터 넘어온 토큰을 검증한다.
+		String authToken = request.getHeader("Authorization");
+		System.out.println("authToken" + authToken);
+		
+		// 토큰이 존재한다면 토큰 값이 유효한지를 체크한다.
+		if(authToken != null) {
+			authToken = authToken.replace("Bearer ", "");
+			
+			try {
+				Claims claims = JwtTokenHelper.parseClaims(authToken).getBody();
+				String id = claims.getId();
+				String issuer = claims.getIssuer();
+				String subject = claims.getSubject();
+				System.out.println("id: " + id + " issuer: " + issuer + " subject: " + subject);
+				result = Constants.RESULT_VAL_OK;
+				resultMsg = "회원정보 Token 검증에 성공 하였습니다.";
+			} catch (ExpiredJwtException expException) {
+				expException.printStackTrace();
+				resultMsg = "Token 의 유효기간이 만료 되었습니다. 다시 발급 받으세요."; 
+			} catch (Exception e) {
+				e.printStackTrace();
+				resultMsg = e.getMessage();
+			}
+		}
+		
+		// 실패한 이유에 대해서 response를 준다.
+		StringBuilder jsonMsg = new StringBuilder();
+		jsonMsg.append("{");
+		jsonMsg.append("\"result\"");
+		jsonMsg.append(":");
+		jsonMsg.append("\"");
+		jsonMsg.append(Constants.RESULT_VAL_FAIL);
+		jsonMsg.append("\"");
+		jsonMsg.append(", \"resultMsg\"");
+		jsonMsg.append(":");
+		jsonMsg.append("\"");
+		jsonMsg.append(resultMsg);
+		jsonMsg.append("\"");
+		jsonMsg.append("}");
+		
+		PrintWriter writer = response.getWriter();
+		writer.write(jsonMsg.toString());
+		writer.flush();
+		writer.close();
+		
+		return false;
 	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		super.postHandle(request, response, handler, modelAndView);
+		System.out.println("TokenInterceptor postHandle() ===> ");
 	}
 	
-	
 
-}
+}	
---
+++ HelloSpring/src/main/java/com/hello/hellospring/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.hello.hellospring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.hello.hellospring.common.interceptor.TokenCheckInterceptor;
+
+// 스프링의 웹 설정을 담당하는 클래스다 라 고 지정해주는 어노테이션이 있다.
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	// 인터셉터를 등록
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new TokenCheckInterceptor())
+		.addPathPatterns("/**") // 모든 url에 인터셉터를 걸겠다.
+		.excludePathPatterns("/getLoginToken") // 토큰을 발행해주는 url은 예외처리 (인터셉터를 타지 않는다)
+		.excludePathPatterns("/login/**") // 로그인 하기 전의 url 예외
+		.excludePathPatterns("/board/*Form*")
+		.excludePathPatterns("/insertBoard");
+	}	
+	
+}
