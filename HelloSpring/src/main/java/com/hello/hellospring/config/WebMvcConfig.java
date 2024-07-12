6b94958f8020ff06ddf8730d89f347a4aedd4c57
안지애 <rozioh@naver.com>
2024-07-12 18:57:13 +0900

    boardList.html 구현, WebMvcConfig에서 해당 url 인터셉터 제외 처리

--- HelloSpring/src/main/java/com/hello/hellospring/config/WebMvcConfig.java
+++ HelloSpring/src/main/java/com/hello/hellospring/config/WebMvcConfig.java
@@ -18,7 +18,8 @@ public class WebMvcConfig implements WebMvcConfigurer{
 		.addPathPatterns("/**") // 모든 url에 인터셉터를 걸겠다.
 		.excludePathPatterns("/getLoginToken") // 토큰을 발행해주는 url은 예외처리 (인터셉터를 타지 않는다)
 		.excludePathPatterns("/login/**") // 로그인 하기 전의 url 예외
-		.excludePathPatterns("/board/*Form*");
+		.excludePathPatterns("/board/*Form*")
+		.excludePathPatterns("/board/boardList.html");
 		
 	}	
 	
